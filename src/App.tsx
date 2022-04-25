e228abc7708f29a72320c4f58cb385ae5640fe3c
anuaroslan <anuaroslan@gmail.com>
2022-04-26 02:25:22 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Wheel } from "react-custom-roulette";
+import { Button, Center, Container, Group, Modal } from "@mantine/core";
+
+const data = [
+  {
+    option: "Try again",
+    style: { backgroundColor: "pink", textColor: "black" },
+  },
+  {
+    option: "You won RM100!",
+    style: { backgroundColor: "green", textColor: "black" },
+  },
+  {
+    option: "You won a voucher!",
+    style: { backgroundColor: "lightBlue", textColor: "black" },
+  },
+  {
+    option: "Try again",
+    style: { backgroundColor: "pink", textColor: "black" },
+  },
+  {
+    option: "You won RM100!",
+    style: { backgroundColor: "green", textColor: "black" },
+  },
+  {
+    option: "Better luck next time :(!",
+    style: { backgroundColor: "pink", textColor: "black" },
+  },
+  {
+    option: "You won a voucher!",
+    style: { backgroundColor: "lightBlue", textColor: "black" },
+  },
+];
 
 function App() {
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+  const [prizeModalOpen, setPrizeModalOpen] = useState(false);
+  const [prizeText, setPrizeText] = useState("Try again");
+
+  const handleSpinClick = () => {
+    const newPrizeNumber = Math.floor(Math.random() * data.length);
+    setPrizeNumber(newPrizeNumber);
+    setMustSpin(true);
+  };
+
+  const handlePrizeText = () => {
+    if (prizeNumber === 0 || prizeNumber === 3) {
+      setPrizeText("Try again");
+    } else if (prizeNumber === 1 || prizeNumber === 4) {
+      setPrizeText("You won RM100!");
+    } else if (prizeNumber === 2 || prizeNumber === 6) {
+      setPrizeText("You won a voucher!");
+    } else {
+      setPrizeText("Better luck next time!");
+    }
+  };
+
+  const handleStopSpin = () => {
+    handlePrizeText();
+    setPrizeModalOpen(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Wheel
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={data}
+          backgroundColors={["#3e3e3e", "#df3428"]}
+          textColors={["#ffffff"]}
+          fontSize={12}
+          onStopSpinning={handleStopSpin}
+        />
+        <Group position="center" mt={20}>
+          <Button onClick={handleSpinClick}>Spin</Button>
+        </Group>
+      </Container>
+      <Modal
+        opened={prizeModalOpen}
+        onClose={() => setPrizeModalOpen(false)}
+        title="Prize modal"
+      >
+        <Center>{prizeText}</Center>
+      </Modal>
+    </>
   );
 }
 
